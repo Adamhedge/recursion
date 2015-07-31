c5e07fb0165489dcb975a8f6126e801a82adb1a9
Adam Hedgpeth <Adamhedge@gmail.com>
2015-07-30 22:00:45 -0700

    Just establishing an algorithm

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,13 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //if it's an array, slice off the first value
+  //if it's an object, grab the first property
+  //if it's undefined or a function, don't do anything with it
+  //create the string value of the thing - handle null.
+  //create the JSON 
+  if (obj == null)
+  	console.log("Null");
+  else console.log("The base value of this object is: " + obj.toString());
+  return JSON.stringify(obj);
 };
