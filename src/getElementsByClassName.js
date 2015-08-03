f12cd2395851823a4b51483ce5ac1a757240b00c
Adam Hedgpeth <Adamhedge@gmail.com>
2015-08-03 13:54:51 -0700

    Figured out the second problem too!  So happy to be done with getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+
 var getElementsByClassName = function(className
 ){
-  // your code here
+  //console.log(document.getElementsByClassName(className));
+  var body = document.body;
+  var results = [];
+
+  var getElementsbyClassNameFromObject = function(domObject){
+  	console.log(domObject.hasOwnProperty('classList'));
+  	if(domObject['classList'] !== undefined){
+      if (domObject.classList.length > 0 && domObject.classList.contains(className)){
+        results.push(domObject);
+      }
+    }
+    if(domObject.hasChildNodes()){
+	  for(var i = 0; i < domObject.childNodes.length; i ++){
+	    getElementsbyClassNameFromObject(domObject.childNodes[i]);
+      }
+    }
+  };
+  
+  getElementsbyClassNameFromObject(body);
+  return results;
+  return Array.prototype.slice.apply(document.getElementsByClassName(className));
+ 
 };
