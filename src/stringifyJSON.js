96d4c763c204cfd5494fa97fc2fe159b62378d2d
Adam Hedgpeth <Adamhedge@gmail.com>
2015-08-02 20:48:37 -0700

    I should have done more partial commits, but finished stringifyJSON!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,14 +3,54 @@
 
 // but you don't so you're going to write it from scratch:
 
+//This is just some code to handle strings and integers, and nulls.
+var stringifyPrims = function(obj){
+  if (obj == null){
+    console.log("null");
+    return "null";
+  }
+  if (typeof obj != 'obj' && obj != null){
+    console.log("It's not an object: " + obj);
+    if (typeof obj == 'string')
+      return ('"' + obj.toString() + '"');
+    else return obj.toString();
+  }
+};
+
+
+//This basically detects an array or object, adds the framing syntax, and loops through the contents, calling itself recursively.
+//If this doesn't identify an array or an object, it assumes that it's a primitive and returns a primitive value with syntax decoration via stringifyPrims.
 var stringifyJSON = function(obj) {
-  //if it's an array, slice off the first value
-  //if it's an object, grab the first property
-  //if it's undefined or a function, don't do anything with it
-  //create the string value of the thing - handle null.
-  //create the JSON 
-  if (obj == null)
-  	console.log("Null");
-  else console.log("The base value of this object is: " + obj.toString());
+  
+  var result = "";
+  var firstRun = true;
+  if (obj instanceof Array){
+    result = result + "[";
+      for(var i = 0; i < obj.length; i ++){
+        result = result + stringifyJSON(obj[i]);
+        if (i != obj.length - 1)
+          result = result + ",";
+      }
+    result = result + "]";
+  }
+  else if (obj instanceof Object){
+    result = result + "{";
+    for(var k in obj){
+      if((typeof obj[k] !== "undefined") && (typeof obj[k] !== "function")){
+        if (firstRun == true){
+          firstRun = false;
+        }
+        else result = result + ',';
+        result = result + '"' + k + '":' + stringifyJSON(obj[k]);
+      }
+    }
+    result = result + "}";
+
+  }
+  else return stringifyPrims(obj);
+  return result;
+
   return JSON.stringify(obj);
 };
+
+
